ce6c3731829abff961224d4b3b815ec71bf9b105
unknown <256pasternakmarina@gmail.com>
2020-01-14 22:26:47 +0200

    v3

--- functions.js
+++ functions.js
@@ -1,4 +1,18 @@
-function slider()
+var slides = document.getElementsByClassName("slid");
+var currentSlide = 0;
+
+
+function nextSlide(){
+	goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(n)
 {
-    
+	slides[currentSlide].className = "slid";
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = "slid showing";
 }
