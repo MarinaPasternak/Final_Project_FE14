dcd8dfffa480d8146663b4d60d257f68f7cd5f59
unknown <256pasternakmarina@gmail.com>
2020-01-16 15:56:25 +0200

    v4

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 var slides = document.getElementsByClassName("slid");
 var currentSlide = 0;
 
-
 function nextSlide(){
 	goToSlide(currentSlide+1);
 }
@@ -15,4 +14,32 @@ function goToSlide(n)
 	slides[currentSlide].className = "slid";
 	currentSlide = (n+slides.length)%slides.length;
 	slides[currentSlide].className = "slid showing";
+}
+
+
+
+function paginationButton(el)
+{
+	n =  el.id;
+	var totalNumberOfPages = document.getElementsByClassName('page').length;
+    for(var i=1; i<=totalNumberOfPages; i++){
+
+        if (document.getElementById('page'+i)) {
+
+			document.getElementById('page'+i).style.display='none';
+			document.getElementById(i).style.backgroundColor = '#020f18';
+		}
+		
+    }
+	if (document.getElementById('page'+n)) {
+
+		document.getElementById('page'+n).style.display='block';
+		document.getElementById(n).style.backgroundColor = 'rgba(240, 36, 36, 0.849)';
+	} 
+	return parseInt(n)
+}	
+function paginationArrowNext()
+{
+   num = paginationButton(el)
+
 }
