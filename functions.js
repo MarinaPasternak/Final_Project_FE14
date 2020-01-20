5d98175ad46889697a9cf5793c47bdce0cb530a3
unknown <256pasternakmarina@gmail.com>
2020-01-20 18:29:16 +0200

    v5

--- functions.js
+++ functions.js
@@ -17,29 +17,52 @@ function goToSlide(n)
 }
 
 
-
 function paginationButton(el)
 {
 	n =  el.id;
-	var totalNumberOfPages = document.getElementsByClassName('page').length;
-    for(var i=1; i<=totalNumberOfPages; i++){
+	return n;
+	
+}
 
-        if (document.getElementById('page'+i)) {
+function pagination(el)
+	{
+		n=paginationButton(el);
+		var totalNumberOfPages = document.getElementsByClassName('page').length;
+		for(var i=1; i<=totalNumberOfPages; i++){
 
-			document.getElementById('page'+i).style.display='none';
-			document.getElementById(i).style.backgroundColor = '#020f18';
+			if (document.getElementById('page'+i)) {
+
+				document.getElementById('page'+i).style.display='none';
+				document.getElementById(i).style.backgroundColor = '#020f18';
+			}
+			
+		}
+		if (document.getElementById('page'+n)) {
+
+			document.getElementById('page'+n).style.display='block';
+			document.getElementById(n).style.backgroundColor = 'rgba(240, 36, 36, 0.849)';
+		} 
+	
+	}
+
+	
+	    function webUrlChange() { 
+		var divId = window.location.hash.toString();
+		let match = Array.from(document.querySelectorAll("main div"));
+		var len = document.querySelectorAll("main div").length;
+		let showDiv =  document.querySelectorAll(divId+" div");
+		var showDivLen = document.querySelectorAll(divId+" div").length;
+		for(var i=0; i<len; i++)
+		{
+			match[i].style.display='none';
+		}
+		var showBlock=document.getElementById(divId.slice(1)).style.display='block';
+		for(var i=0; i<showDivLen; i++)
+		{
+			showDiv[i].style.display= "";
 		}
 		
-    }
-	if (document.getElementById('page'+n)) {
-
-		document.getElementById('page'+n).style.display='block';
-		document.getElementById(n).style.backgroundColor = 'rgba(240, 36, 36, 0.849)';
-	} 
-	return parseInt(n)
-}	
-function paginationArrowNext()
-{
-   num = paginationButton(el)
-
-}
+	}
+	window.onhashchange =webUrlChange;
+	document.addEventListener("DOMContentLoaded",webUrlChange);
+	
